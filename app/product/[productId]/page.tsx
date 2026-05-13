759ede197ee4d581f57c30f780bb9742cc5dc456
Rimon Mahamud Rony <rimonronycste11@gmail.com>
2026-05-14 01:16:25 +0600

    ci: dynamic routes

---
+++ app/product/[productId]/page.tsx
@@ -0,0 +1,19 @@
+export default async function ProductDetails(
+    { params
+        
+    }: {
+            params: Promise<{ productId: string }>;
+    }
+) {
+    const productId = (await params).productId ;
+    return (
+      <div className="w-full h-screen items-center flex flex-row justify-center bg-gray-100">
+        <div className="text-slate-500">
+          <h1 className="text-2xl font-bold text-cyan-500">{productId} Page</h1>
+          <h2 className="text-lg">
+            This is the {productId} page of the Next.js application.
+          </h2>
+        </div>
+      </div>
+    );
+}

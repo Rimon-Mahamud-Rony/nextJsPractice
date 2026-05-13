a83337d8ab57a275f06ab9dd62b5f24652040740
Rimon Mahamud Rony <rimonronycste11@gmail.com>
2026-05-13 09:37:40 +0600

    project started

---
+++ app/about/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+    return (
+        <div className="w-full h-screen items-center flex flex-row justify-center bg-gray-100">
+            <div className="text-slate-500">
+                <h1 className="text-2xl font-bold">About Page</h1>
+                <h2 className="text-lg">This is the about page of the Next.js application.</h2>
+            </div>
+        </div>
+    )
+}
---
+++ app/profile/page.tsx
@@ -0,0 +1,10 @@
+export default function Profile() {
+    return (
+        <div className="w-full h-screen items-center flex flex-row justify-center bg-gray-100">
+            <div className="text-slate-500">
+                <h1 className="text-2xl font-bold">Profile Page</h1>
+                <h2 className="text-lg">This is the profile page of the Next.js application.</h2>
+            </div>
+        </div>
+    )
+}

b14ebee3834163ffab3a880ddf0e1004a501ac57
Rimon Mahamud Rony <rimonronycste11@gmail.com>
2026-05-26 09:43:36 +0600

    ci: active links

--- app/(auth)/AuthTemplate.tsx
+++ app/(auth)/AuthTemplate.tsx
@@ -5,14 +5,20 @@ type AuthLayoutProps = Readonly<{
   body: string;
 }>;
 
+
+
 export default function AuthTemplate({
   children,
   heading,
   subject,
   body,
 }: AuthLayoutProps) {
+
+  const forgot_paass = heading === "Forgot Password"; 
+  const isLogin = heading === "Welcome Back";
+
   return (
-    <div className="w-full flex justify-center overflow-y-auto mb-20 min-h-[86vh]">
+    <div className="w-full flex justify-center overflow-y-auto mb-20 min-h-[75vh]">
       <div className="w-full max-w-2xl rounded-xl bg-white shadow-sm border border-slate-200 p-6">
         {/* Top Badge */}
         <div className="bg-cyan-700 text-white p-1 rounded-md w-fit px-4 text-center mb-3 text-sm font-medium">
@@ -20,22 +26,28 @@ export default function AuthTemplate({
         </div>
 
         {/* Heading */}
-        <h1 className="text-2xl font-bold tracking-tight text-cyan-700 leading-snug text-center">
-          {heading}
-        </h1>
+        {forgot_paass === true ? (
+          <h1 className="text-2xl font-bold tracking-tight text-red-500 leading-snug text-center">
+            {heading}
+          </h1>
+        ) : isLogin === true ? (
+          <h1 className="text-2xl font-bold tracking-tight text-green-500 leading-snug text-center">
+            {heading}
+          </h1>
+        ) : (
+          <h1 className="text-2xl font-bold tracking-tight text-cyan-700 leading-snug text-center">
+            {heading}
+          </h1>
+        )}
 
         {/* Subject */}
-        <h2 className="mt-2 text-sm text-slate-500 text-center">
-          {subject}
-        </h2>
+        <h2 className="mt-2 text-sm text-slate-500 text-center">{subject}</h2>
 
         {/* Divider */}
         <div className="w-full border-t border-cyan-500 my-4"></div>
 
         {/* Body */}
-        <p className="text-xs leading-7 text-slate-600 text-center">
-          {body}
-        </p>
+        <p className="text-xs leading-7 text-slate-600 text-center">{body}</p>
 
         {/* Page Content */}
         <div className="flex items-center justify-center">{children}</div>
--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,26 +1,44 @@
+"use client";
+
 import Link from "next/link";
+import {usePathname} from "next/navigation";
 import CommonPageDesign from "../components/CommonPageDesign";
+import { use } from "react";
+import link from "next/link";
+
+const navLinks = [
+  { name: "Login", href: "/login" },
+  { name: "Register", href: "/register" },
+  { name: "Forgot Password", href: "/forgot-password" },
+];
+
 export default function AuthLayout({
     children,
 }: Readonly<{
   children: React.ReactNode;
-
 }>) {
+  const pathname = usePathname();
   return (
     <CommonPageDesign>
-        <div className="w-full flex justify-center gap-4 py-6 mt-10">
-        <Link href="/login" className="px-5 py-2  transition">
-          Login
-        </Link>
+      <div className="w-full flex justify-center gap-4 py-6 mt-10">
+        
+        {navLinks.map((link) =>{
+            
+            const isActive = pathname === link.href || (pathname.startsWith(link.href) && link.href !== "/");
 
-        <Link href="/register" className="px-5 py-2  transition">
-          Register
-        </Link>
+            return (
+              <Link
+                key={link.href}
+                href={link.href}
+                className={`px-5 py-2  transition ${isActive ? "bg-cyan-800 text-white transition" : "hover:bg-gray-200 hover:text-black"}`}
+              >
+                {link.name}
+              </Link>
+            )
+          }
+        )}
 
-        <Link href="/forgot-password" className="px-5 py-2  transition">
-          Forgot Password
-        </Link>
-        </div>
+      </div>
         {children}
     </CommonPageDesign>
   );
